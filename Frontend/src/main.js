884d5746cf2b67f04166ebafb2ade6dd008bca72
lizolla <77739101+lizolla@users.noreply.github.com>
2021-02-28 19:38:34 +0200

    Update main.js

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,3 +1,4 @@
+/* eslint-env jquery */
 /**
  * Created by chaika on 25.01.16.
  */
@@ -6,10 +7,8 @@ $(function(){
     //This code will execute when the page is ready
     var PizzaMenu = require('./pizza/PizzaMenu');
     var PizzaCart = require('./pizza/PizzaCart');
-    var Pizza_List = require('./Pizza_List');
-
+    var Pizza_List = require('./Pizza_List.js');
     PizzaCart.initialiseCart();
     PizzaMenu.initialiseMenu();
-
 
 });
